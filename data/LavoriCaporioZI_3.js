a5fc432ca7af919128099fa84b085f989a326746
cittasottoscaccosrl <85392183+cittasottoscaccosrl@users.noreply.github.com>
2021-06-18 15:40:57 +0200

    Add files via upload

---
+++ data/LavoriCaporioZI_3.js
@@ -0,0 +1,15 @@
+var json_LavoriCaporioZI_3 = {"type":"FeatureCollection","name":"LavoriCaporioZI_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
+
+{"type":"Feature","properties":{"Descrizion":"Riqualificazione area verde","Anno":"2021","Foto 1":null,"Foto 2":null},"geometry":{"type":"Point","coordinates":[14.399822168530099,41.549223353879952]}},
+
+{"type":"Feature","properties":{"Descrizion":"Riqualificazione piazzetta centro borgo","Anno":"2021","Foto 1":"49a.png","Foto 2":"49b.png"},"geometry":{"type":"Point","coordinates":[14.398063303830565,41.548099133581985]}},
+
+{"type":"Feature","properties":{"Descrizion":"Riqualificazione ponte","Anno":"2021","Foto 1":"C:\\Città Sotto Scacco\\Comune di Macchiagodena\\LLPP\\foto per webmap\\50a.png","Foto 2":"C:\\Città Sotto Scacco\\Comune di Macchiagodena\\LLPP\\foto per webmap\\50b.png"},"geometry":{"type":"Point","coordinates":[14.399405216090672,41.548681737617606]}},
+
+{"type":"Feature","properties":{"Descrizion":"Potenziamento opere idriche","Anno":"2021","Foto 1":"C:\\Città Sotto Scacco\\Comune di Macchiagodena\\LLPP\\foto per webmap\\51a.png","Foto 2":"C:\\Città Sotto Scacco\\Comune di Macchiagodena\\LLPP\\foto per webmap\\51b.png"},"geometry":{"type":"Point","coordinates":[14.399511081679139,41.549241194045145]}},
+
+{"type":"Feature","properties":{"Descrizion":"Pulizia canale in terra","Anno":"2021","Foto 1":null,"Foto 2":null},"geometry":{"type":"Point","coordinates":[14.397662831291042,41.549373338591323]}},{"type":"Feature","properties":{"Descrizion":"Recupero canale in cemento","Anno":"2021","Foto 1":null,"Foto 2":null},"geometry":{"type":"Point","coordinates":[14.399280254244404,41.548186135424068]}},
+
+{"type":"Feature","properties":{"Descrizion":"Potenziamento delle opere di deflusso delle acque","Anno":"2021","Foto 1":null,"Foto 2":null},"geometry":{"type":"Point","coordinates":[14.400962308983242,41.548812206985133]}},
+
+{"type":"Feature","properties":{"Descrizion":"Potenziamento delle opere di deflusso delle acque","Anno":"2021","Foto 1":null,"Foto 2":null},"geometry":{"type":"Point","coordinates":[14.40516304962207,41.548853478591873]}}]}
